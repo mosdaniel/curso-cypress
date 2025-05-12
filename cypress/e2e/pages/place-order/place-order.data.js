8ada245eb57ce4e62a849d3830a27aa6b2553423
daniel <danielsteven2080@gmail.com>
2025-05-12 10:28:08 -0600

    creacion de la pagina place order

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,19 @@
+export class PlaceOrderData{
+
+   static get testDate(){
+        return{
+        name:"Test name",
+        country:"united states",
+        city:"boston",
+        card:"5555555555554444",
+        month:12,
+        year:2024
+
+        }    
+
+   }
+
+
+
+
+}
---
+++ cypress/e2e/pages/place-order/place-order.methods.js
@@ -0,0 +1,32 @@
+import { PlaceOrderElements } from "./place-order.elements";
+
+export class PlaceOrderMethods{
+
+    static insertName(name){
+        PlaceOrderElements.textboxes.name.invoke("val",name)
+    }
+     static insertCountry(country){
+        PlaceOrderElements.textboxes.country.invoke("val",country)
+    }
+    static insertCity(city){
+        PlaceOrderElements.textboxes.city.invoke("val",city)
+    }
+        static insertCard(card){
+        PlaceOrderElements.textboxes.card.invoke("val",card)
+    }
+          static insertMonth(month){
+        PlaceOrderElements.textboxes.month.invoke("val",month)
+    }
+          static insertYear(year){
+        PlaceOrderElements.textboxes.year.invoke("val",year)
+    }
+ 
+    static clickOnCloseButton(){
+        PlaceOrderElements.buttons.close.click();
+    }
+        static clickOnPurchaseButton(){
+        PlaceOrderElements.buttons.purchase.click();
+    }
+
+
+}
